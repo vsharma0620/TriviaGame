4336e87bed84b83bca9995bc7ecb1a8b88399b0e
Vanita <vanitasharma_16@hotmail.com>
2018-09-14 00:13:52 -0700

    adding timer

--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -7,3 +7,55 @@ function Question(text, choices, answer) {
 Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
+
+ //  Set our number counter to 100.
+ var number = 100;
+
+ //  Variable that will hold our interval ID when we execute
+ //  the "run" function
+ var intervalId;
+
+ //  When the resume button gets clicked, execute the run function.
+ $("#resume").on("click", run);
+
+ //  The run function sets an interval
+ //  that runs the decrement function once a second.
+ //  *****BUG FIX******** 
+ //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+ function run() {
+   clearInterval(intervalId);
+   intervalId = setInterval(decrement, 1000);
+ }
+
+ //  The decrement function.
+ function decrement() {
+
+   //  Decrease number by one.
+   number--;
+
+   //  Show the number in the #show-number tag.
+   $("#show-number").html("<h2>" + number + "</h2>");
+
+
+   //  Once number hits zero...
+   if (number === 0) {
+
+     //  ...run the stop function.
+     stop();
+
+     //  Alert the user that time is up.
+     alert("Time Up!");
+   }
+ }
+
+ //  The stop function
+ function stop() {
+
+   //  Clears our intervalId
+   //  We just pass the name of the interval
+   //  to the clearInterval function.
+   clearInterval(intervalId);
+ }
+
+ //  Execute the run function.
+ run();
